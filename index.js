36fb01ae28cf9144b68661fa8e13bb9d82402e03
Viktor Palyuha <viktorpalyuha@gmail.com>
2020-09-16 01:07:42 +0300

    now user cannot add empty item to the list

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ let input = document.querySelector("input");
 let addButton = document.querySelector("button");
 let ulList = document.querySelector("ul");
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", (event) => {
     event.preventDefault();
+    let text = input.value;
+
+    if(text.trim() === "") { 
+        alert("Please, type something")
+        return;
+    }
+
     let newLiElement = document.createElement("li");
     let newSpan = document.createElement("span");
     newSpan.innerHTML = "\u00D7";
     newSpan.className = "close";
 
-    newLiElement.innerHTML = input.value;
+    newLiElement.innerHTML = text;
     newLiElement.append(newSpan);
     ulList.append(newLiElement);
     input.value = "";
